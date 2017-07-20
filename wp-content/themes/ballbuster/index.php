6dc38927d6fc9c3d3dd87f1771733995af5a5a38
Brandon LaDuke <brandon@brandonladuke.com>
2017-07-19 21:35:31 -0400

    Made the last 4 new posts be in the same row. And some bug fixes

--- wp-content/themes/ballbuster/index.php
+++ wp-content/themes/ballbuster/index.php
@@ -29,106 +29,95 @@
         <p>By Brandon LaDuke</p>
         <p>July 18, 2017</p>
       </div>
-    
+
   </div>
   <div class="new-post">
     <div class="new-post-img"></div>
-    <div class="">
+
       <div class="new-post-des">
         <h1>This is the Newest post on Ballbuster 2017</h1>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         <p>By Brandon LaDuke</p>
         <p>July 18, 2017</p>
       </div>
-    </div>
+
   </div>
   <div class="new-post">
     <div class="new-post-img"></div>
-    <div class="">
+
       <div class="new-post-des">
         <h1>This is the Newest post on Ballbuster 2017</h1>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         <p>By Brandon LaDuke</p>
         <p>July 18, 2017</p>
       </div>
-    </div>
+
   </div>
   <div class="new-post">
     <div class="new-post-img"></div>
-    <div class="">
+
       <div class="new-post-des">
         <h1>This is the Newest post on Ballbuster 2017</h1>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         <p>By Brandon LaDuke</p>
         <p>July 18, 2017</p>
       </div>
-    </div>
+
   </div>
   <div class="new-post">
     <div class="new-post-img"></div>
-    <div class="">
+
       <div class="new-post-des">
         <h1>This is the Newest post on Ballbuster 2017</h1>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         <p>By Brandon LaDuke</p>
         <p>July 18, 2017</p>
       </div>
-    </div>
+
   </div>
   <div class="new-post">
     <div class="new-post-img"></div>
-    <div class="">
+
       <div class="new-post-des">
         <h1>This is the Newest post on Ballbuster 2017</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <p class="post-excert">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         <p>By Brandon LaDuke</p>
         <p>July 18, 2017</p>
       </div>
-    </div>
+
   </div>
   <div class="new-post">
     <div class="new-post-img"></div>
-    <div class="">
+
       <div class="new-post-des">
         <h1>This is the Newest post on Ballbuster 2017</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <p class="post-excert">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         <p>By Brandon LaDuke</p>
         <p>July 18, 2017</p>
       </div>
-    </div>
+
   </div>
   <div class="new-post">
     <div class="new-post-img"></div>
-    <div class="">
+
       <div class="new-post-des">
         <h1>This is the Newest post on Ballbuster 2017</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <p class="post-excert">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         <p>By Brandon LaDuke</p>
         <p>July 18, 2017</p>
       </div>
-    </div>
+
   </div>
   <div class="new-post">
     <div class="new-post-img"></div>
-    <div class="">
+
       <div class="new-post-des">
         <h1>This is the Newest post on Ballbuster 2017</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <p class="post-excert">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         <p>By Brandon LaDuke</p>
         <p>July 18, 2017</p>
       </div>
-    </div>
-  </div>
-  <div class="new-post">
-    <div class="new-post-img"></div>
-    <div class="">
-      <div class="new-post-des">
-        <h1>This is the Newest post on Ballbuster 2017</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        <p>By Brandon LaDuke</p>
-        <p>July 18, 2017</p>
-      </div>
-    </div>
+
   </div>
 
   </div>
