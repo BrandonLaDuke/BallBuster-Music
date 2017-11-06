1ce8b8d72a59c730c95942d46899ed2e4105c68c
Brandon LaDuke <brandon@brandonladuke.com>
2017-11-06 18:36:24 -0500

    article styles applied, basic

--- wp-content/themes/ballbuster-V2/header.php
+++ wp-content/themes/ballbuster-V2/header.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
     <link rel="stylesheet" type="text/css" href="<?php bloginfo('template_directory');?>/styles.css" />
-
+    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
 
     <?php wp_head();?>
   </head>
--- wp-content/themes/ballbuster-V2/single.php
+++ wp-content/themes/ballbuster-V2/single.php
@@ -146,7 +146,7 @@ if ( has_post_format( 'video' )) {
 
         <h4><?php $post_date = get_the_date( 'F j, Y' ); echo $post_date ?></h4>
       </div>
-      <div>
+      <div class="postPhoto-container">
         <div class="postPhoto" style="background-image: url(<?php the_post_thumbnail_url(); ?>)"></div>
       </div>
     </div>
@@ -154,27 +154,93 @@ if ( has_post_format( 'video' )) {
 
 
 		<div class="contPad">
-      <div class="mobileAd-container">
-        <div class="mobileAd">
- 	       <!-- BallBuster -->
+      <div class="article-grid">
+        <div class="mobileAd-container">
+          <div class="mobileAd">
+   	       <!-- BallBuster -->
+           <img src="http://placehold.it/728x90" alt="">
+            <!-- <ins class="adsbygoogle"
+                 style="display:block"
+                 data-ad-client="ca-pub-8458877549325827"
+                 data-ad-slot="9908918197"
+                 data-ad-format="auto"></ins> -->
+          </div> <!-- .mobileAd -->
+        </div>
+
+         <div class="main-column">
+  	       <div>
+    				<?php
+    						get_template_part('content-single');
+    				?>
+    				<?php
+    					endwhile; endif;
+    				?>
+    			</div>
+
+          <div class="author-block">
+            <div class="left">
+              <img src="<?php echo get_avatar_url( get_the_author_meta( 'ID' ), 32 ); ?>" alt="author">
+            </div>
+            <div class="right">
+              <div>
+                <h3>By <?php the_author_posts_link(); ?></h3>
+                <p>Published: <?php $post_date = get_the_date( 'F j, Y' ); echo $post_date ?></p>
+                <p>Category: <?php $category = get_the_category();
+                $firstCategory = $category[0]->cat_name;
+                 echo $firstCategory; ?></p>
+              </div>
+            </div>
+          </div>
+
+          <div class="comments">
+            <div class="diqus">
+              <div id="disqus_thread"></div>
+            </div>
+          </div>
+        </div> <!-- .main-column -->
+
+        <div class="sidebar">
+
+
+<h2 class="sidebar-title">Also on BallBuster Music:</h2>
+<?php
+  // the query
+  $recentPosts = new WP_Query(array('post_type'=>'post', 'post_status'=>'publish', 'posts_per_page'=>-8)); ?>
+  <?php if ( $recentPosts->have_posts() ) : while ( $recentPosts->have_posts() ) : $recentPosts->the_post();
+
+  //count == number of posts to display
+  static $count = 0;
+  if ($count == "8") { break; }
+    else { ?>
+      <a href="<?php the_permalink(); ?>" class="related-post">
+        <img src="<?php the_post_thumbnail_url(); ?>" alt="">
+        <h2><?php the_title(); ?></h2>
+        <h4>By <?php the_author(); ?></h4>
+      </a>
+      <?php if ($count == "3" || $count == "6") {
+        ?>
           <ins class="adsbygoogle"
                style="display:block"
+               data-ad-format="fluid"
+               data-ad-layout-key="-8i+1w-dq+e9+ft"
                data-ad-client="ca-pub-8458877549325827"
-               data-ad-slot="9908918197"
-               data-ad-format="auto"></ins>
-        </div> <!-- .mobileAd -->
+               data-ad-slot="6650224050"></ins>
+          <script>
+               (adsbygoogle = window.adsbygoogle || []).push({});
+          </script>
+        <?php
+      }?>
+    <?php $count++; } ?>
+  <?php endwhile; ?>
+
+<?php endif; ?>
+
+
+
+
+        </div>
       </div>
 
-       <div class="main-column">
-	       <div>
-  				<?php
-  						get_template_part('content-single');
-  				?>
-  				<?php
-  					endwhile; endif;
-  				?>
-  			</div>
-      </div> <!-- .main-column -->
     </div> <!-- .contPad -->
 
 
